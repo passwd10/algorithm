79ec246ed4c626d1ce86ba6d4bde1db0b483f898
passwd10 <parkinseo94@naver.com>
2020-03-30 22:24:59 +0900

    Delete package file

--- fifthCompetition/competition.test.js
+++ fifthCompetition/competition.test.js
@@ -4,32 +4,33 @@
 1. "BACDE"를 split으로 나눈다.
 2. "C", "B", "D"를 findIndex로 돌린다
 3. sort를 했을때 이전과 다르다면 스킬 x
+
+주의: 선행 스킬트리에 있는 스킬이 아닌 스킬로만 구성된 스킬트리가 있을 수 있음.
+
+1. 먼저 skill_trees에 있는 각 문자열들을 선행스킬 문자만으로 구성된 문자열로 변환된 배열로 만든다.
+2. 그 후 배열 안의 문자들 중 선행스킬 문자열의 부분집합인 문자열들만 걸러낸다.
+3. 걸러낸 배열의 길이를 리턴한다.
+
 */
+const checkPriority = (firstStr, secondStr) =>
+  secondStr.split('')
+    .map(v => firstStr.split('').findIndex(e => e === v));
 
-const solution = (skill, skill_trees) => {
-  return skill_trees.map(v => checkSkill(skill.split(''), v.split('')))
-    // .filter((arr, i) => i !== arr.length && arr[arr.findIndex(e => e === -1)] > 0)
-    .map(e => removeMinus(e))
-    .filter(skillArr => skillArr.join('') == skillArr.sort((a,b) => a - b).join('')).length;
+
+const canLearnSkillCount = (skill, skill_trees) => {
+  return skill_trees.length;
 }
 
-const checkSkill = (skillArr, skillTreeArr) => {
-  return skillArr.map(v => skillTreeArr.findIndex(skill => v === skill));
-}
-
-const removeMinus = arr => arr.filter(v => v !== -1);
-
-test('solution', () => {
-  expect(solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"])).toBe(2);
-  expect(solution("ABD", ["BACDE", "CBADF", "AECB", "BDA"])).toBe(1);
-  expect(solution("EBD", ["BACDE", "CBADF", "AECB", "BDA"])).toBe(2);
+test('canLearnSkillCount', () => {
+  expect(canLearnSkillCount("ABC", ["A", "B"])).toBe(2);
+  expect(canLearnSkillCount("ABC", ["A", "B", "ACB"])).toBe(2);
+  expect(canLearnSkillCount("CBD", ["EFG"])).toBe(1);
+  // expect(canLearnSkillCount("CBD" ["BACDE", "CBADF", "AECB", "BDA"])).toBe(2);
 })
 
-test('checkSkill', () => {
-  expect(checkSkill(["C", "B", "D"], ["B", "A", "C", "D", "E"])).toEqual([2, 0, 3]);
-  expect(checkSkill(["A", "B", "D"], ["A", "E", "C", "B"])).toEqual([0, 3, -1]);
+test.only('checkPriority', () => {
+  expect(checkPriority("ABC", "ACB")).toEqual([0, 2, 1]);
+  expect(checkPriority("ABC", "ADBC")).toEqual([0, -1, 1, 2]);
+  expect(checkPriority("CBD", "EFG")).toEqual([-1, -1, -1]);
 })
 
-test('removeMinus', () => {
-  expect(removeMinus([0, 3, -1])).toEqual([0, 3]);
-})
---
+++ poor-runner/README.md
@@ -0,0 +1,17 @@
+# [완주하지 못한 선수](https://programmers.co.kr/learn/courses/30/lessons/42576?language=javascript)
+
+## 1. 이해
+
+- completion의 길이는 participant의 길이보다 1 작다
+- 참자가의 이름은 모두 소문자
+- 동명이인 있을 수 있음
+
+## 2. 계획
+
+1. completion배열을 반복문을 돌려서 participant배열의 값중 일치하는 값이 있는지 찾는다
+2. 일치하는 값을 찾으면 participant에서 해당 값을 삭제한다.
+3. 남은 participant값을 반환한다.
+
+## 3. 실행
+
+## 4. 반성
