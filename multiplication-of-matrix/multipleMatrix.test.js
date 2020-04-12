99e117c02925faeab55353a5638f7bf6a180e10a
passwd10 <parkinseo94@naver.com>
2020-04-12 23:22:30 +0900

    Solve 행렬의 곱셈 problem by javascript

--- multiplication-of-matrix/multipleMatrix.test.js
+++ multiplication-of-matrix/multipleMatrix.test.js
@@ -1,27 +1,17 @@
 const solution = (arr1, arr2) => {
-  let result = new Array(arr1.length).fill(null).map(_ => Array());
+  let result = Array(arr1.length).fill().map(_ => Array(arr2[0].length).fill(0));
 
   for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      result[i][0] = arr1[i][0] * arr2[0][0] + arr1[i][1] * arr2[1][0];
-      result[i][1] = arr1[i][0] * arr2[0][1] + arr1[i][1] * arr2[1][1];
+    for (let j = 0; j < arr1[0].length; j++) {
+      for (let r = 0; r < arr2[0].length; r++) {
+        result[i][r] += arr1[i][j] * arr2[j][r];
+      }
     }
   }
   return result;
 }
 
-// result[0][0] = arr1[0][0] * arr2[0][0] + arr1[0][1] * arr2[1][0];
-// result[0][1] = arr1[0][0] * arr2[0][1] + arr1[0][1] * arr2[1][1];
-
-// result[1][0] = arr1[1][0] * arr2[0][0] + arr1[1][1] * arr2[1][0];
-// result[1][1] = arr1[1][0] * arr2[0][1] + arr1[1][1] * arr2[1][1];
-
-// result[2][0] = arr1[2][0] * arr2[0][0] + arr1[2][1] * arr2[1][0];
-// result[2][1] = arr1[2][0] * arr2[0][1] + arr1[2][1] * arr2[1][1];
-
-
-
 test('solution', () => {
   expect(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]])).toEqual([[15, 15], [15, 15], [15, 15]]);
-  // expect(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]],[[5, 4, 3], [2, 4, 1], [3, 1, 1]])).toEqual([[22, 22, 11], [36, 28, 18], [29, 20, 14]]);
+  expect(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]],[[5, 4, 3], [2, 4, 1], [3, 1, 1]])).toEqual([[22, 22, 11], [36, 28, 18], [29, 20, 14]]);
 })
